b8b3b8efb877ee8c2248571c5cc0d5473b4a8d53
hafizhpebrian <hafizhpebrian_2226250077@mhs.mdp.ac.id>
2024-04-06 22:36:36 +0700

    update navbar

---
+++ lib/screens/notifikasi.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Notifikasi extends StatefulWidget {
+  const Notifikasi({super.key});
+
+  @override
+  State<Notifikasi> createState() => _NotifikasiState();
+}
+
+class _NotifikasiState extends State<Notifikasi> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: Text(
+          'notifikasi',
+        ),
+      ),
+    );
+  }
+}
