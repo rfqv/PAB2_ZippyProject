d774d467cc65192666e09a9cd28994137678adf1
MuhammadSyahrizalHidayat <muhammadsyahrizalhidayat_2226250086@mhs.mdp.ac.id>
2024-06-03 21:22:35 +0700

    up

---
+++ lib/screens/cuitan.dart
@@ -0,0 +1,63 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class AddCuitanScreen extends StatefulWidget {
+  const AddCuitanScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AddCuitanScreen> createState() => _AddCuitanScreenState();
+}
+
+class _AddCuitanScreenState extends State<AddCuitanScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _CuitanController = TextEditingController();
+
+  Future<void> _addTweet() async {
+    if (_formKey.currentState!.validate()) {
+      final cuitanText = _CuitanController.text;
+      final newcuitan = Cuitan(
+        text: cuitanText,
+        userId: 'your_user_id', 
+        timestamp: DateTime.now(),
+      );
+
+      await FirebaseFirestore.instance.collection('cuitan').add(newcuitan.toJson());
+      Navigator.pop(context);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add cuitan'),
+        actions: [
+          TextButton(
+            onPressed: _addTweet,
+            child: const Text('Cuitan'),
+          ),
+        ],
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: TextFormField(
+            controller: _CuitanController,
+            decoration: const InputDecoration(
+              hintText: 'What\'s happening?',
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter a tweet';
+              }
+              return null;
+            },
+            maxLines: null,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
--- lib/screens/thread_image_quote.dart
+++ lib/screens/thread_image_quote.dart
@@ -1,35 +1,108 @@
-import 'dart:io';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
-class ThreadImageQuote extends StatefulWidget {
-  const ThreadImageQuote({super.key});
+class AddQuoteThreadScreen extends StatefulWidget {
+  const AddQuoteThreadScreen({Key? key}) : super(key: key);
 
-  Future<void> uploadImage() async {
-  final picker = ImagePicker();
-  final pickedFile = await picker.pickImage(source: ImageSource.gallery);
-
-  if (pickedFile != null) {
-    final file = File(pickedFile.path);
-    final storageRef = FirebaseStorage.instance.ref().child('thread_images/${DateTime.now()}.jpg');
-    final uploadTask = storageRef.putFile(file);
-
-    await uploadTask.whenComplete(() {
-      print('Image uploaded successfully');
-    });
-  } else {
-    print('No image selected');
-  }
-}
   @override
-  State<ThreadImageQuote> createState() => _ThreadImageQuoteState();
+  State<AddQuoteThreadScreen> createState() => _AddQuoteThreadScreenState();
 }
 
-class _ThreadImageQuoteState extends State<ThreadImageQuote> {
+class _AddQuoteThreadScreenState extends State<AddQuoteThreadScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _quoteController = TextEditingController();
+  XFile? _selectedImage;
+
+  Future<void> _selectImage() async {
+    final picker = ImagePicker();
+    final pickedImage = await picker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      _selectedImage = pickedImage;
+    });
+  }
+
+  Future<void> _createThread() async {
+    if (_formKey.currentState!.validate()) {
+      if (_selectedImage != null) {
+        final imageUrl = await _uploadImage(_selectedImage!);
+        final quoteText = _quoteController.text;
+
+        final firstTweet = Cuitan(
+          text: '', 
+          userId: 'your_user_id', 
+          timestamp: DateTime.now(),
+          imageUrl: imageUrl,
+        );
+
+        final secondTweet = Cuitan(
+          text: quoteText,
+          userId: 'your_user_id', 
+          timestamp: DateTime.now(),
+          isQuote: true, 
+          quoteTweetId: firstTweet.id, 
+        );
+
+        await FirebaseFirestore.instance.collection('Cuitan').add(firstTweet.toJson());
+        await FirebaseFirestore.instance.collection('Cuitan').add(secondTweet.toJson());
+
+        Navigator.pop(context);
+      }
+    }
+  }
+
+  Future<String> _uploadImage(XFile imageFile) async {
+    final storageRef = FirebaseStorage.instance.ref().child('Cuitan').child(DateTime.now().toString());
+    final uploadTask = storageRef.putFile(imageFile);
+    await uploadTask.whenComplete(() async {
+      return await storageRef.getDownloadURL();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add Quote Thread'),
+        actions: [
+          TextButton(
+            onPressed: _createThread,
+            child: const Text('Post'),
+          ),
+        ],
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _quoteController,
+                decoration: const InputDecoration(
+                  hintText: 'Enter your quote',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a quote';
+                  }
+                  return null;
+                },
+                maxLines: null,
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: _selectImage,
+                child: const Text('Select Image'),
+              ),
+              const SizedBox(height: 16.0),
+              if (_selectedImage != null)
+                Image.file(_selectedImage!.path),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
